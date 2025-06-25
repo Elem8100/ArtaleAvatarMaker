e5bf368679b72228ab6bad213ad6d1b993cbb2ed
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2025-06-25 22:16:47 +0800

    Add files via upload

--- MapleNecrocer/Client/MapleChair.cs
+++ MapleNecrocer/Client/MapleChair.cs
@@ -321,7 +321,7 @@ public class MapleChair : SpriteEx
 
     public override void DoDraw()
     {
-      MainForm.Instance.Text= CharacterAction;
+    
         if(Game.Player.Action==CharacterAction)
             base.DoDraw();
 
